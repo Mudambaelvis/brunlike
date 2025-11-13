5e6a10a6a8539d49fb1cc4f23612ca9e6aeb627e
freshcodesElvo <elvismudamba@gmail.com>
2025-11-13 12:50:40 +0300

    fix: clean index.html and rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/brunlike/', // 👈 must match your GitHub repo name
+  base: '/brunlike/', 
 })
