c9f99ff238db39fdb3d046f146c365c9b6cbdd4a
freshcodesElvo <elvismudamba@gmail.com>
2025-11-13 13:18:59 +0300

    feat: updated build output to docs folder for github pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/brunlike/', 
+  build: { // 💡 ADD THIS SECTION
+    outDir: 'docs', // Tells Vite to output to the 'docs' folder
+  }
 })
