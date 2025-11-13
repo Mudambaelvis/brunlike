69176c541e08687c657c6de5c0309e43ef269629
freshcodesElvo <elvismudamba@gmail.com>
2025-11-13 12:35:06 +0300

    APP.JSX FIXED

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // ONLY import what is needed (Routes and Route)
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <Router>
+    <> {/* Use a Fragment or <div> instead of the Router */}
       <Navbar />
       <main className="pt-16">
         <Routes>
@@ -19,6 +19,6 @@ export default function App() {
         </Routes>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
